c9e86dc5edaaab19dc96434b275b65eb4e4dfe9d
abakarovmaks <abakarovmaks@gmail.com>
2024-02-29 15:50:09 +0200

    bootstrap dropdown

---
+++ tests/bootstrapDropdown.spec.js
@@ -0,0 +1,24 @@
+const { test, expect } = require('@playwright/test');
+
+test('multi select', async ({ page }) => {
+  await page.goto('https://www.jquery-az.com/boots/demo.php?ex=63.0_2');
+
+  //   await page.getByTestId('dropdown').click();
+  await page.locator('.btn-group').click();
+
+  //   const options = await page.locator('ul>li input');
+  //     expect(options).toHaveCount(11);
+
+  //   const options = await page.$$('ul>li input');
+  //   expect(options.length).toBe(11);
+
+  const options = await page.$$('ul>li label');
+  for (const opt of options) {
+    const value = await opt.textContent();
+    if (value.includes('Oracle')) {
+      await opt.click();
+    }
+  }
+
+  await page.waitForTimeout(2000);
+});
